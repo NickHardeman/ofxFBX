28a82732451533e89dbd245247a24d5a97a352c7
NickHardeman <nickhardeman@gmail.com>
2015-08-23 14:38:29 -0400

    change to ofApp

--- ExampleBoneControl/src/ofApp.cpp
+++ ExampleBoneControl/src/ofApp.cpp
@@ -1,17 +1,17 @@
-#include "testApp.h"
+#include "ofApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup() {
+void ofApp::setup() {
     ofDisableArbTex();
     
     ofxFBXSceneSettings settings;
     
-    string filename = "humanoid.fbx";
+    string filename = "astroBoy_walk.fbx";
     
     if( scene.load(filename, settings) ) {
-        cout << "testApp :: loaded the scene OK" << endl;
+        cout << "ofApp :: loaded the scene OK" << endl;
     } else {
-        cout << "testApp :: Error loading the scene" << endl;
+        cout << "ofApp :: Error loading the scene" << endl;
     }
     
     cam.lookAt( ofVec3f(0,0,0) );
@@ -29,7 +29,7 @@ void testApp::setup() {
 }
 
 //--------------------------------------------------------------
-void testApp::update() {
+void ofApp::update() {
     
 //    light.setPosition( cos(ofGetElapsedTimef()) * 20.f, sin(ofGetElapsedTimef()*4) * 50 + 10, sin(ofGetElapsedTimef()) * 20.f + 100);
     
@@ -42,7 +42,7 @@ void testApp::update() {
 }
 
 //--------------------------------------------------------------
-void testApp::draw() {
+void ofApp::draw() {
     
     ofSetColor(255, 255, 255);
     
@@ -113,7 +113,7 @@ void testApp::draw() {
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key) {
+void ofApp::keyPressed(int key) {
     if(scene.getNumAnimations() > 1) {
         if(key == OF_KEY_DOWN ) {
             int newAnimIndex = fbxMan.getCurrentAnimationIndex()+1;
@@ -134,7 +134,7 @@ void testApp::keyPressed(int key) {
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key) {
+void ofApp::keyReleased(int key) {
     if(key == ' ') {
         fbxMan.getCurrentAnimation().togglePlayPause();
     }
@@ -150,37 +150,37 @@ void testApp::keyReleased(int key) {
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y) {
+void ofApp::mouseMoved(int x, int y) {
     
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button) {
+void ofApp::mouseDragged(int x, int y, int button) {
     
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button)    {
+void ofApp::mousePressed(int x, int y, int button)    {
     
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button) {
+void ofApp::mouseReleased(int x, int y, int button) {
     
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h) {
+void ofApp::windowResized(int w, int h) {
     
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg) {
+void ofApp::gotMessage(ofMessage msg) {
     
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo) {
+void ofApp::dragEvent(ofDragInfo dragInfo) {
     
 }
 
