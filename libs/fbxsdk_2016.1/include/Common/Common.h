f1e4a54849f18006137d0f026ffab5a07efc14c2
NickHardeman <nickhardeman@gmail.com>
2019-09-24 17:42:56 -0400

    vs libs

--- libs/fbxsdk_2016.1/include/Common/Common.h
+++ libs/fbxsdk_2016.1/include/Common/Common.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include "fbxsdk.h"
+#include "ofLog.h"
 
 #ifdef IOS_REF
 #undef  IOS_REF
@@ -26,7 +27,11 @@ void InitializeSdkObjects(FbxManager*& pManager, FbxScene*& pScene)
         FBXSDK_printf("Error: Unable to create FBX Manager!\n");
         exit(1);
     }
-	else FBXSDK_printf("Autodesk FBX SDK version %s\n", pManager->GetVersion());
+    else {
+        if(ofGetLogLevel() == OF_LOG_VERBOSE) {
+            FBXSDK_printf("Autodesk FBX SDK version %s\n", pManager->GetVersion());
+        }
+    }
     
 	//Create an IOSettings object. This object holds all import/export settings.
 	FbxIOSettings* ios = FbxIOSettings::Create(pManager, IOSROOT);
@@ -147,39 +152,39 @@ bool LoadScene(FbxManager* pManager, FbxDocument* pScene, const char* pFilename)
         return false;
     }
     
-    FBXSDK_printf("FBX file format version for this FBX SDK is %d.%d.%d\n", lSDKMajor, lSDKMinor, lSDKRevision);
+    if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("FBX file format version for this FBX SDK is %d.%d.%d\n", lSDKMajor, lSDKMinor, lSDKRevision);
     
     if (lImporter->IsFBX())
     {
-        FBXSDK_printf("FBX file format version for file '%s' is %d.%d.%d\n\n", pFilename, lFileMajor, lFileMinor, lFileRevision);
+        if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("FBX file format version for file '%s' is %d.%d.%d\n\n", pFilename, lFileMajor, lFileMinor, lFileRevision);
         
         // From this point, it is possible to access animation stack information without
         // the expense of loading the entire file.
         
-        FBXSDK_printf("Animation Stack Information\n");
+        if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("Animation Stack Information\n");
         
         lAnimStackCount = lImporter->GetAnimStackCount();
         
-        FBXSDK_printf("    Number of Animation Stacks: %d\n", lAnimStackCount);
-        FBXSDK_printf("    Current Animation Stack: \"%s\"\n", lImporter->GetActiveAnimStackName().Buffer());
-        FBXSDK_printf("\n");
+        if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("    Number of Animation Stacks: %d\n", lAnimStackCount);
+        if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("    Current Animation Stack: \"%s\"\n", lImporter->GetActiveAnimStackName().Buffer());
+        if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("\n");
         
         for(i = 0; i < lAnimStackCount; i++)
         {
             FbxTakeInfo* lTakeInfo = lImporter->GetTakeInfo(i);
             
-            FBXSDK_printf("    Animation Stack %d\n", i);
-            FBXSDK_printf("         Name: \"%s\"\n", lTakeInfo->mName.Buffer());
-            FBXSDK_printf("         Description: \"%s\"\n", lTakeInfo->mDescription.Buffer());
+            if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("    Animation Stack %d\n", i);
+            if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("         Name: \"%s\"\n", lTakeInfo->mName.Buffer());
+            if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("         Description: \"%s\"\n", lTakeInfo->mDescription.Buffer());
             
             // Change the value of the import name if the animation stack should be imported
             // under a different name.
-            FBXSDK_printf("         Import Name: \"%s\"\n", lTakeInfo->mImportName.Buffer());
+            if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("         Import Name: \"%s\"\n", lTakeInfo->mImportName.Buffer());
             
             // Set the value of the import state to false if the animation stack should be not
             // be imported.
-            FBXSDK_printf("         Import State: %s\n", lTakeInfo->mSelect ? "true" : "false");
-            FBXSDK_printf("\n");
+            if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("         Import State: %s\n", lTakeInfo->mSelect ? "true" : "false");
+            if(ofGetLogLevel() == OF_LOG_VERBOSE) FBXSDK_printf("\n");
         }
         
         // Set the import states. By default, the import states are always set to
