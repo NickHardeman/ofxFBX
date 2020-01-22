8fbe04b90b5250642956b39ffd1067084bda69ba
themancalledjakob <j.schloetter@gmail.com>
2020-01-22 11:17:24 +0100

    add linux & linux64 libs, update addon_config.mk

--- addon_config.mk
+++ addon_config.mk
@@ -68,4 +68,10 @@ ios:
 	ADDON_LDFLAGS = -lxml2 -liconv
 	ADDON_INCLUDES_EXCLUDE = libs/libfbxsdk_2019.5/include/libxml2/%
 
+linux:
+	ADDON_LDFLAGS = -lxml2
+	ADDON_INCLUDES_EXCLUDE = libs/libfbxsdk_2019.5/include/libxml2/%
 
+linux64:
+	ADDON_LDFLAGS = -lxml2
+	ADDON_INCLUDES_EXCLUDE = libs/libfbxsdk_2019.5/include/libxml2/%
